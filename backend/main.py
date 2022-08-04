6ab7c536c69c1cdfc1535fe0ad014a9027cc8040
aka-vm <vineetmahajan2000@gmail.com>
2022-08-04 23:13:14 +0530

    code restructured

--- backend/main.py
+++ backend/main.py
@@ -1,47 +1,23 @@
-import os
-import base64, io
-import PIL.Image as Image
-
-from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 
+from .models import DogImage
 from .model_utils import get_clf_model, clf_breed
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 clf_model = get_clf_model()
-
-class CVisionOppration(str, Enum):
-    predict = "predict"
-    detect = "detect"
-
-class DogImage(BaseModel):
-    image_bytes: bytes
-    top_n: int = 3
-
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    data = {
-        "message": "Hello World",
-        }
-    return data
+    return {"message": "Hello World",}
 
-@app.post("/vision/{operation}")
-async def vision(operation: CVisionOppration, dog_image: DogImage):
-
-    if operation == CVisionOppration.predict:
-        image_bytes = dog_image.image_bytes
-        image = Image.open(io.BytesIO(base64.decodebytes(image_bytes)))
-        respon = clf_breed(clf_model, image, dog_image.top_n)
-        return JSONResponse(respon)
-
-    if operation == CVisionOppration.detect:
-        return {"message": "Yolo"}
-
+@app.post("/predict-breed/")
+async def predict_breed(dog_image: DogImage):
+    image_bytes = dog_image.image_bytes
+    respon = clf_breed(clf_model, image_bytes, dog_image.top_n)
+    return JSONResponse(respon)
--- backend/model_utils.py
+++ backend/model_utils.py
@@ -1,36 +1,50 @@
-import os
+import base64, io, json, os
 import numpy as np
 from PIL import Image
-import json
 from keras.models import load_model
 
 MODEL_INPUT_SHAPE = (224, 224)
 
-def get_model(model_path):
+
+def get_model(
+        model_path: str
+        ):
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
     model = load_model(model_path)
     return model
 
 def get_clf_model():
     model_path = 'models/InceptionResNetV2.h5'
-    return get_model(model_path)
+    model = get_model(model_path)
+    return model
 
-def clf_breed(model, image: Image, top_n: int=3) -> dict:
-    image = preprocess_image(image)
-    prediction_arr = model.predict(image)[0]
+def clf_breed(
+        model,
+        image_bytes: bytes,
+        top_n: int=3
+        ) -> dict:
+    """
+    This function takes in a model and an image-64-bit data and returns the top N breeds with probability percentages.
+    """
+    image = preprocess_image(image_bytes)
+    prediction = model.predict(image)[0]
 
     with open("breeds_dict.json") as f:
         breed_dict = json.load(f)
 
-    top_n_pred_score = np.argsort(prediction_arr)[-top_n:][::-1]
-    top_n_pred_breed = {breed_dict[str(i)]: float(prediction_arr[i]) for i in top_n_pred_score}
-
+    top_n_pred_score = np.argsort(prediction)[-top_n:][::-1]
+    top_n_pred_breed = {
+                breed_dict[str(i)]: float(prediction[i])
+                for i in top_n_pred_score
+            }
     return top_n_pred_breed
 
-def preprocess_image(image: Image) -> np.ndarray:
+def preprocess_image(image_bytes: bytes) -> np.ndarray:
     """
     Preprocess the image to be used for prediction.
     """
     # Resize the image to the model input shape
+    image = Image.open(io.BytesIO(base64.decodebytes(image_bytes)))
     image = image.resize(MODEL_INPUT_SHAPE).convert('RGB')
     image = np.array([np.array(image)]) / 255
     return image
