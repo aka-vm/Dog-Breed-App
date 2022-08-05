39ee8e2dfd879d331312ade0507fb31e43e3f5bd
aka-vm <vineetmahajan2000@gmail.com>
2022-08-05 07:06:07 +0530

    complete app runs, static files changes needed

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, status
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from .models import DogImage
 from .model_utils import get_clf_model, clf_breed
@@ -11,13 +12,22 @@ from .model_utils import get_clf_model, clf_breed
 clf_model = get_clf_model()
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World",}
+templates = Jinja2Templates(directory="backend/templates")
 
-@app.post("/predict-breed/")
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/predict-breed/")
+async def predict_breed(request: Request):
+    return templates.TemplateResponse("result.html", {"request": request})
+
+@app.post("/api/predict-breed/")
 async def predict_breed(dog_image: DogImage):
     image_bytes = dog_image.image_bytes
     respon = clf_breed(clf_model, image_bytes, dog_image.top_n)
     return JSONResponse(respon)
+
