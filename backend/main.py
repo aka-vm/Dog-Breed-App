c224a173567008e952323685b60c661c4662ceba
aka-vm <vineetmahajan2000@gmail.com>
2023-07-17 19:17:29 +0530

    backend functionality for YOLOv8

--- backend/main.py
+++ backend/main.py
@@ -4,15 +4,22 @@ from fastapi import FastAPI, Request, Response, status
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import DogImage
-from model_utils import get_clf_model, clf_breed
+from model_utils import get_clf_model, clf_breed, get_det_model, det_breed
 
-
-clf_model = get_clf_model()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 templates = Jinja2Templates(directory="backend/templates")
 
@@ -28,7 +35,15 @@ def predict_breed(request: Request):
 @app.post("/api/predict-breed/")
 async def predict_breed(dog_image: DogImage):
     image_bytes = dog_image.image_bytes
-    respon = clf_breed(clf_model, image_bytes, dog_image.top_n)
+    task = dog_image.task
+    if task == "clf":
+        clf_model = get_clf_model()
+        respon = clf_breed(clf_model, image_bytes, dog_image.top_n)
+
+    elif task == "det":
+        det_model = get_det_model()
+        respon = det_breed(det_model, image_bytes, dog_image.conf_thres, dog_image.iou_thres)
+
     return JSONResponse(respon)
 
 if __name__ == "__main__":
--- backend/model_utils.py
+++ backend/model_utils.py
@@ -2,6 +2,8 @@ import base64, io, json, os
 import numpy as np
 from PIL import Image
 from keras.models import load_model
+from ultralytics import YOLO
+import cv2
 
 MODEL_INPUT_SHAPE = (224, 224)
 
@@ -18,11 +20,42 @@ def get_clf_model():
     model = get_model(model_path)
     return model
 
+def get_det_model():
+    model_path = 'model-binaries/best.torchscript'
+    model = YOLO(model_path, task="detect")
+    return model
+
+def det_breed(
+    model,
+    image_bytes: bytes,
+    conf_thres: float=0.25,
+    iou_thres: float=0.45,
+) -> dict:
+    """
+    This function takes in a model and an image-64-bit data and returns bounding boxes and classes.
+    """
+    image = preprocess_image(image_bytes, task="det")
+    # results = model.predict(image, conf_thres=conf_thres, iou_thres=iou_thres)[0].boxes
+    results = model.predict(image)[0].boxes
+
+    data = {
+        "cls": results.cls.tolist(),
+        "xyxyn": results.xyxyn.tolist(),
+        "conf": results.conf.tolist(),
+    }
+    with open("model-binaries/breeds_dict.json") as f:
+        breed_dict = json.load(f)
+
+    data["cls"] = [breed_dict[str(int(i))] for i in data["cls"]]
+
+
+    return data
+
 def clf_breed(
-        model,
-        image_bytes: bytes,
-        top_n: int=3
-        ) -> dict:
+    model,
+    image_bytes: bytes,
+    top_n: int=3
+) -> dict:
     """
     This function takes in a model and an image-64-bit data and returns the top N breeds with probability percentages.
     """
@@ -39,12 +72,22 @@ def clf_breed(
             }
     return top_n_pred_breed
 
-def preprocess_image(image_bytes: bytes) -> np.ndarray:
+def preprocess_image(image_bytes: bytes, task="clf") -> np.ndarray:
     """
     Preprocess the image to be used for prediction.
     """
     # Resize the image to the model input shape
     image = Image.open(io.BytesIO(base64.decodebytes(image_bytes)))
-    image = image.resize(MODEL_INPUT_SHAPE).convert('RGB')
-    image = np.array([np.array(image)]) / 255
+    if task == "clf":
+        image = image.resize(MODEL_INPUT_SHAPE).convert('RGB')
+        image = np.array([np.array(image)]) / 255
+    elif task == "det":
+        w, h = image.size
+        if max(h, w) > 640:
+            scale = 640 / max(h, w)
+            new_h, new_w = int(h * scale), int(w * scale)
+            image = image.resize(new_w, new_h).convert('RGB')
+        image = np.array(image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
     return image
